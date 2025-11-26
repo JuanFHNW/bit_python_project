c200a634e39dbbd8f858b87a2e0f67cfcea0576c
F2rn6nd0 <fernando.maurachergarcia@students.fhnw.ch>
2025-11-26 14:58:55 +0100

    commit addTask

--- taskFunctions/addTask.py
+++ taskFunctions/addTask.py
@@ -3,12 +3,15 @@ import re
 def addTask():
     while True:
         userInputDate = input("Write down the date you want to add to your task in the follwing format: \nyyyy.mm.dd\n")
-        year, month, day = userInputDate.split('.')
-        if len(year) == 4 and year.isdigit() and len(month) == 2 and month.isdigit() and len(day) == 2 and day.isdigit():  
-            print(f"userinput data:  {userInputDate}")
-            break
-        else:
-            print("Wrong input please use the format: yyyy.mm.dd")
+        try:
+            year, month, day = userInputDate.split('.')
+            if len(year) == 4 and year.isdigit() and len(month) == 2 and month.isdigit() and len(day) == 2 and day.isdigit():  
+                print(f"userinput data:  {userInputDate}")
+                break
+            else:
+                print("Wrong input please use the format: yyyy.mm.dd")
+        except ValueError:
+            print("Wrong input!") 
 
     taskDescription = input("Write down the description of the task:\n")
 
